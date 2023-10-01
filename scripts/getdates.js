4015bf30ff0af206240d3b378fcf9f678d4191f6
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-09-30 21:36:56 -0600

    hamburger menu + a few adjustments

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,16 @@
+// Nav
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+
+// Date
 const today= new Date();
 document.querySelector('#currentyear').textContent = today.getFullYear();
 document.querySelector('#lastmodified').textContent = document.lastModified;
+
+
