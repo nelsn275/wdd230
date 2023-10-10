9ba56db013853c6957151f68b1d93438bf7e4e8e
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-10-10 15:18:51 -0600

    big-nav

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -3,9 +3,9 @@ document.querySelector("last-modified").innerHTML = document.lastModified;
 // Hamburger Menu
 function makeHamburger() {
     var x = document.getElementById("links");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+        if (x.style.display === "block") {
+            x.style.display = "none";
+          } else {
+            x.style.display = "block";
+        }
+}
