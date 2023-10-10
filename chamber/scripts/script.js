8376b4e1e2a02b8a36fdff0b212802cc0ec4d64b
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-10-09 22:33:25 -0600

    base chamber home page skeleton

---
+++ chamber/scripts/script.js
@@ -0,0 +1,11 @@
+document.querySelector("last-modified").innerHTML = document.lastModified;
+
+// Hamburger Menu
+function makeHamburger() {
+    var x = document.getElementById("links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
