114c9811b1b2c1d88ab1f4dd909b2880634b383c
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-10-16 22:50:43 -0600

    dark mode

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -9,3 +9,23 @@ function makeHamburger() {
             x.style.display = "block";
         }
 }
+
+// Dark mode
+var darkButton = document.querySelector("#dark-mode")
+darkButton.addEventListener('click', ()=>{
+  if (darkButton.textContent == 'DARK'){
+    document.documentElement.style.setProperty('--text-color', '#ffe8d6')
+    document.documentElement.style.setProperty('--background-color', '#6b705c')
+    document.documentElement.style.setProperty('--hover-background', '#b7b7a4')
+    document.documentElement.style.setProperty('--hover-text', 'black')
+    darkButton.textContent = 'LIGHT'
+  }
+  else{
+    document.documentElement.style.setProperty('--text-color', 'black')
+    document.documentElement.style.setProperty('--background-color', '#b7b7a4')
+    document.documentElement.style.setProperty('--hover-background', '#6b705c')
+    document.documentElement.style.setProperty('--hover-text', '#ffe8d6')
+    darkButton.textContent = 'DARK'
+  }
+
+})
