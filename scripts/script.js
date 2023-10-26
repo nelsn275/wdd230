f4023d3cf7e1b09e6c500977aa53fbbb0a51c64f
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-10-26 15:29:27 -0600

    site visits counter implemented

--- scripts/script.js
+++ scripts/script.js
@@ -8,9 +8,29 @@ hambutton.addEventListener('click', () => {
 });
 
 
+
 // Date
 const today= new Date();
 document.querySelector('#currentyear').textContent = today.getFullYear();
 document.querySelector('#lastmodified').textContent = document.lastModified;
+
+
+
+// Site Visits
+const VISITS_KEY = 'site-visits'
+
+function getSiteVisits(){
+	let currValue = localStorage.getItem(VISITS_KEY)
+	let siteVisits = 1
+
+	if (currValue != null){
+		siteVisits = parseInt(currValue) + 1
+	}
+	localStorage.setItem(VISITS_KEY, `${siteVisits}`)
+	return siteVisits
+}
+
+document.getElementById('visitcount').textContent = `${getSiteVisits()}`
+
 
 
