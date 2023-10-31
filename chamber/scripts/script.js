06d368b66b51d280b093672358a929091128d71a
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-10-31 15:27:22 -0600

    discover & nav updates

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -43,22 +43,21 @@ function displayWelcome() {
   let today = new Date()
   if (lastVisitValue != null){
 
-    lastVisit = parseInt(LAST_VISIT_DATE_KEY)
-    daysSinceLastVisit = Math.floor((today - lastVisit) / DAY_IN_MILLIS)
+    daysSinceLastVisit = Math.floor((today.getTime() - lastVisitValue) / DAY_IN_MILLIS)
 
     if (daysSinceLastVisit == 0){
       message = 'Back so soon? Awesome!'
-
     }
     else{
       if(daysSinceLastVisit == 1){
         message = 'You last visited 1 day ago'
       }
       else{
-        mesage = `You last visited ${daysSinceLastVisit} days ago`
+        message = `You last visited ${daysSinceLastVisit} days ago`
       }
     }
   }
+  console.log(daysSinceLastVisit)
   localStorage.setItem(LAST_VISIT_DATE_KEY, `${today.getTime()}`)
   return message;
 }
