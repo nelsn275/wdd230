1860a32e4304fc3d8e5f6b856e59b37644e12edc
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-10-31 13:06:09 -0600

    color contrast resolved

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -15,7 +15,7 @@ var darkButton = document.querySelector("#dark-mode")
 darkButton.addEventListener('click', ()=>{
   if (darkButton.textContent == 'DARK'){
     document.documentElement.style.setProperty('--text-color', '#ffe8d6')
-    document.documentElement.style.setProperty('--background-color', '#6b705c')
+    document.documentElement.style.setProperty('--background-color', '#464B3F')
     document.documentElement.style.setProperty('--hover-background', '#b7b7a4')
     document.documentElement.style.setProperty('--hover-text', 'black')
     darkButton.textContent = 'LIGHT'
@@ -23,7 +23,7 @@ darkButton.addEventListener('click', ()=>{
   else{
     document.documentElement.style.setProperty('--text-color', 'black')
     document.documentElement.style.setProperty('--background-color', '#b7b7a4')
-    document.documentElement.style.setProperty('--hover-background', '#6b705c')
+    document.documentElement.style.setProperty('--hover-background', '#464B3F')
     document.documentElement.style.setProperty('--hover-text', '#ffe8d6')
     darkButton.textContent = 'DARK'
   }
