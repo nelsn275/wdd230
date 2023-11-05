cde65839edb6caeaed145bed50d4031822b550c5
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-11-04 21:40:46 -0600

    form complete

--- scripts/script.js
+++ scripts/script.js
@@ -32,5 +32,24 @@ function getSiteVisits(){
 
 document.getElementById('visitcount').textContent = `${getSiteVisits()}`
 
+// Site Rating
+const rating = document.getElementById("rating")
+rating.addEventListener('change', () => {
+    document.getElementById("currentrating").innerHTML = rating.value;
+})
+
+// Password Match
+const confirm = document.getElementById("confirm")
+confirm.addEventListener('blur', ()=>{
+    const password= document.getElementById("password")
+    if (password.value != confirm.value){
+        document.getElementById("message").innerHTML="Passwords do not match!"
+        password.focus()    
+    }
+    else{
+        document.getElementById("message").innerHTML=""
+    }
+})
+
 
 
