b0ef7d40a84f2f78f1a11f268be57bd6f2b8a71a
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-11-07 15:37:13 -0700

    new ward members

--- chamber/scripts/join-chmaber.js
+++ chamber/scripts/join-chmaber.js
@@ -2,5 +2,7 @@ let joinButton = document.getElementById('join')
 
 joinButton.addEventListener('click',  ()=>{
     window.location.href = 'thank-you.html'
-
 })
+
+const formdate = new Date()
+document.getElementById('timestamp').value = formdate.toString()
