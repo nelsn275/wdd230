fca68228bf1d5c4a0dddd651d3c63da901d5f2d1
nelsn275 <131692018+nelsn275@users.noreply.github.com>
2023-11-14 15:22:43 -0700

    more testing

--- chamber/scripts/join-chamber.js
+++ chamber/scripts/join-chamber.js
@@ -2,14 +2,14 @@ let joinButton = document.getElementById('join')
 console.log(window.location.href)
 
 joinButton.addEventListener('click',  ()=>{
-    if (window.location.href == 'index.html' ){
+    if (window.location.href == 'https://nelsn275.github.io/wdd230/chamber/index.html' ){
         window.location.href = 'join.html'
     }
     else{
         window.location.href = 'thank-you.html'
     }
 })
-if (window.location.href == 'join.html'){
+if (window.location.href == 'https://nelsn275.github.io/wdd230/chamber/index.html'){
     const formdate = new Date()
     document.getElementById('timestamp').value = formdate.toString()
 }
